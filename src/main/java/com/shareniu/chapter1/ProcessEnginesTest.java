885b333d38b3571ffc836dcd514866f97bed05e7
sunchuanfu <shohokuf@163.com>
2021-02-13 22:34:24 +0800

    修改mysql密码连接上db后测试类通过了

--- src/main/java/com/shareniu/chapter1/ProcessEnginesTest.java
+++ src/main/java/com/shareniu/chapter1/ProcessEnginesTest.java
@@ -3,6 +3,7 @@ package com.shareniu.chapter1;
 import static org.junit.Assert.assertNotNull;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 /**
@@ -12,24 +13,28 @@ import org.junit.Test;
  *
  */
 public class ProcessEnginesTest {
+	
 	@Test
 	public void getDefaultProcessEngine() {
-		/*ProcessEngineConfiguration p1 = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("com/shareniu/chapter1/activiti.cfg.xml", "processEngineConfiguration");
+		ProcessEngineConfiguration p1 = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("com/shareniu/chapter1/activiti.cfg.xml",
+				"processEngineConfiguration");
 		p1.buildProcessEngine();
-		ProcessEngineConfiguration p2 = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("com/shareniu/chapter1/activiti1.cfg.xml", "processEngineConfiguration");
-		 p2.buildProcessEngine();*/
+//		ProcessEngineConfiguration p2 = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("com/shareniu/chapter1/activiti1.cfg.xml",
+//		"processEngineConfiguration");
+//		 p2.buildProcessEngine();
 	
+		System.out.println(p1);
+		assertNotNull("not null", p1);
 		
-		ProcessEngine  pe = ProcessEngines.getDefaultProcessEngine();
-		System.out.println(pe);
-		assertNotNull("not null", pe); 
+//		ProcessEngine  pe = ProcessEngines.getDefaultProcessEngine();
+//		System.out.println(pe);
+//		assertNotNull("not null", pe); 
 		/* HashMap<String,Integer> map = new HashMap<String, Integer>();  
 	        map.put("a", 4);  
 	        map.put("b", 2);  
 	        map.put("c", 3);  
 	        Integer next = map.values().iterator().next();
 	        System.out.println(next);*/
-		
 	}
 	
 }
--- src/main/java/com/shareniu/chapter2/ProcessEnginesTest.java
+++ src/main/java/com/shareniu/chapter2/ProcessEnginesTest.java
@@ -3,52 +3,65 @@ package com.shareniu.chapter2;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngineInfo;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.impl.util.ReflectUtil;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.UrlResource;
+
 /**
- *  Activiti权威指南书配套代码
- *  
+ * Activiti权威指南书配套代码
+ * 
  * @author shareniu 分享牛 http://www.shareniu.com/
  *
  */
 public class ProcessEnginesTest {
+	
+	@Test
+	public void testBean() {
+		ApplicationContext ac = new GenericXmlApplicationContext("/META-INF/activiti-context.xml");
+		Map<String, ProcessEngine> beanTypes = ac.getBeansOfType(ProcessEngine.class);
+		ProcessEngine pe = beanTypes.values().iterator().next();
+		TaskService ts = pe.getTaskService();
+		System.out.println(ts);
+	}
+	
 	@Test
 	public void getDefaultProcessEngine() {
-		ProcessEngine  processEngine = ProcessEngines.getDefaultProcessEngine();
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 		ProcessEngines.getDefaultProcessEngine();
 		RepositoryService repositoryService2 = processEngine.getRepositoryService();
 		System.out.println(repositoryService2);
 		List<ProcessEngineInfo> processEngineInfos = ProcessEngines.getProcessEngineInfos();
 		for (ProcessEngineInfo processEngineInfo : processEngineInfos) {
-			System.out.println(processEngineInfo.getResourceUrl()+"-->>"+processEngineInfo.getName());
+			System.out.println(processEngineInfo.getResourceUrl() + "-->>" + processEngineInfo.getName());
 		}
 	}
+
 	@Test
 	public void genericXmlApplicationContext() throws Exception {
-		 ClassLoader classLoader = ReflectUtil.getClassLoader();
-		 Enumeration<URL> resources = null;
-		  resources = classLoader.getResources("activiti-context.xml");
-		  while (resources.hasMoreElements()) {
-		        URL resource = resources.nextElement();
-		        ApplicationContext applicationContext = new GenericXmlApplicationContext(new UrlResource(resource));
-		      }
-		
+		ClassLoader classLoader = ReflectUtil.getClassLoader();
+		Enumeration<URL> resources = classLoader.getResources("activiti-context.xml");
+		while (resources.hasMoreElements()) {
+			URL resource = resources.nextElement();
+			ApplicationContext applicationContext = new GenericXmlApplicationContext(new UrlResource(resource));
+		}
 	}
+
 	@Test
 	public void testShareniuLifecycleListener() throws Exception {
-		
-		ProcessEngineConfiguration pc = ProcessEngineConfiguration.
-				createProcessEngineConfigurationFromResource("com/shareniu/chapter2/lifecyclelistener/activiti.cfg.xml");
+		ProcessEngineConfiguration pc = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource(
+				"com/shareniu/chapter2/lifecyclelistener/activiti.cfg.xml");
 		ProcessEngine buildProcessEngine = pc.buildProcessEngine();
-		System.err.println("---->>>>"+buildProcessEngine);
-	} 
+		System.err.println("---->>>>" + buildProcessEngine);
+	}
+
 }
